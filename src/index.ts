4ff3a6b1a72503c7ac9423cc9b7c596ae8949156
couvq <quentincouvelaire@gmail.com>
2022-09-18 16:15:29 -0400

    typescript course

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-const calculateTax = (income: number): number => {
-    return income * .06;
+type Employee = {
+    readonly id: number,
+    name: string,
+    retire: (data: Date) => void
+}
+
+let employee: Employee = {
+    id: 1,
+    name: 'Mosh',
+    retire: (date: Date) => {
+        console.log(date);
+    }
 }
